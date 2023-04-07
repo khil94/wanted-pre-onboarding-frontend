7fcf525861af73369c959de8e3fe679558807ac6
khil94 <khilkhil@naver.com>
2023-04-07 21:12:21 +0900

    [Feat] API 기초 세팅

---
+++ src/api/api.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    console.log("request : ", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (resp: AxiosResponse<any, any>) => {
+    console.log("Response", resp);
+
+    return { status: resp?.status, data: resp.data } as AxiosResponse<any, any>;
+  },
+  (error) => {
+    console.log("Response Error : ", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
---
+++ src/api/constants.ts
@@ -0,0 +1,5 @@
+export const DEV_URL = "http://localhost";
+export const DEV_PORT = 8000;
+
+export const SERVICE_URL = "";
+export const SERVICE_PORT = 0;
