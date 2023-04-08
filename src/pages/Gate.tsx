9021ac9904b04314614129d1ab5cbededd7b8c9c
khil94 <khilkhil@naver.com>
2023-04-09 04:29:27 +0900

    [Feat] 404 Not Found  페이지

--- src/pages/Gate.tsx
+++ src/pages/Gate.tsx
@@ -3,6 +3,7 @@ import SignIn from "../pages/SignIn";
 import Todos from "../pages/Todos";
 import SignUp from "../pages/SignUp";
 import { useEffect, useState } from "react";
+import NotFound from "./NotFound";
 
 export default function Gate() {
   const loc = useLocation();
@@ -27,6 +28,7 @@ export default function Gate() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/todo" element={<Todos />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 }
