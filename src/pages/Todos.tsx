5ce1b415b8b8c4cbc9fe0866b2dd7cb0475deee6
khil94 <khilkhil@naver.com>
2023-04-09 20:52:52 +0900

    [Feat] Todo Component 적용

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,28 +1,67 @@
 import styled from "@emotion/styled";
-import { useState, useEffect } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { IToDoResp } from "../types/api";
-import { getTodoList } from "../api/todoApi";
+import { createTodo, deleteTodo, getTodoList } from "../api/todoApi";
 import Todo from "../components/Todo";
 
 export default function Todos() {
   const [todoList, setTodoList] = useState<IToDoResp[]>([]);
 
+  const handleCreateTodo = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newTodo = e.currentTarget.elements.namedItem(
+      "new-todo"
+    ) as HTMLInputElement;
+    if (newTodo.value) {
+      await createTodo({
+        todo: newTodo.value,
+      });
+      newTodo.value = "";
+      getTodoListFunc();
+    }
+  };
+
+  const getTodoListFunc = async () => {
+    const resp = await getTodoList();
+    if (resp.status === 200) {
+      setTodoList(resp.data);
+    }
+  };
+
   useEffect(() => {
-    const getTodoListFunc = async () => {
-      const resp = await getTodoList();
-      if (resp.status === 200) {
-        setTodoList(resp.data);
-      }
-    };
     getTodoListFunc();
   }, []);
 
   return (
     <Container>
+      <h1>ToDos</h1>
+      <div>
+        <form onSubmit={(e) => handleCreateTodo(e)}>
+          <label htmlFor="new-todo-input">Todo 생성</label>
+          <input
+            id="new-todo-input"
+            data-testid="new-todo-input"
+            name="new-todo"
+            placeholder="New Todo"
+          />
+          <button type="submit" data-testid="new-todo-add-button">
+            추가
+          </button>
+        </form>
+      </div>
       <ul>
         {todoList.map((todo, i) => {
-          return <Todo todo={todo} />;
+          return (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              onDelete={() => {
+                deleteTodo(todo.id);
+                setTodoList(todoList.filter((v) => v.id !== todo.id));
+              }}
+            />
+          );
         })}
       </ul>
     </Container>
