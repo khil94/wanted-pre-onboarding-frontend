baa701d52cf8c511c93bfef09a9532967d46bc45
khil94 <zidru94@gmail.com>
2023-08-16 04:34:35 +0900

    [feat] api response 인터셉트 핸들러 추가

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,7 +1,18 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const api = axios.create({
   baseURL: "https://www.pre-onboarding-selection-task.shop/",
 });
+
+api.interceptors.response.use(
+  (resp: AxiosResponse<any, any>) => {
+    console.log("Response", resp);
+
+    return { status: resp?.status, data: resp.data } as AxiosResponse<any, any>;
+  },
+  (error) => {
+    console.log("Response Error : ", error);
+  }
+);
 
 export default api;
